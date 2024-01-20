cec58882e5c571ad68c4f6d76c72282a98b34b35
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2024-01-20 16:33:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var string="";
+let fact=1;
+var btn=document.getElementById("btn");
+var buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+button.addEventListener('click',(e)=>{
+  buttontext =e.target.innerText;
+    if(buttontext=='='){
+        string=eval(string);
+        document.querySelector('input').value=string;
+    }
+    else if(buttontext=="C"){
+        string=" ";
+        document.querySelector('input').value=string;
+    }
+   else if(buttontext=="Del")
+   {
+    string=string.substring(0,string.length-1);
+    document.querySelector('input').value=string;
+   }
+   else if(buttontext=="%"){
+    
+   }
+    else{
+console.log(e.target)
+string=string+e.target.innerHTML;
+document.querySelector('input').value=string;
+}
+})
+})
+btn.onclick=function()
+{
+    btn.style.transform; 
+    
+}
